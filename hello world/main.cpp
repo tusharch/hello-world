04bd3c837b3591aa47d49edf416e50286df9405d
tusharch <tusharch@umich.edu>
2018-11-28 21:43:07 -0500

    Initial Commit

---
+++ hello world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Tushar Chawla on 11/28/18.
+//  Copyright © 2018 Tushar Chawla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
